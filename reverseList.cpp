e437c47b98c062aa922ee209f7e433a5262e4f75
doveGit <337450871@qq.com>
2019-07-19 09:34:02 +0800

    、

--- reverseList.cpp
+++ reverseList.cpp
@@ -1,3 +1,11 @@
+/*反转链表 */
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
@@ -5,8 +13,9 @@ public:
         ListNode* pre=dummy;
         dummy->next=head;
         
-        for(int i=0;i<m-1;i++)
-            pre=pre->next;
+        for(int i=1;i<m;i++)
+            pre=pre->next;//pre用来标记m位置的前一个节点，将作为反转之后的头结点
+
         ListNode* cur=pre->next;
         for(int i=m;i<n;i++){
             ListNode* t=cur->next;
