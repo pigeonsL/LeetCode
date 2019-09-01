3aef82c38043ebb0ddcaa1cd8234bbff37cb537d
doveGit <337450871@qq.com>
2019-09-01 13:12:57 +0800

    饿汉-单例

--- SingleTon/SingleTonehan.cpp
+++ SingleTon/SingleTonehan.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+using namespace std;
 //饿汉模式,不存在线程安全问题
 class Singleton
 {
 protected:
-    Singleton(){} // 设置为保护便于被继承
+    Singleton(){
+        cout<<"Singleton()..."<<endl;
+    } // 设置为保护便于被继承
+    ~Singleton(){
+        cout<<"~Singleton()..."<<endl;
+    }
 private:
     Singleton(const Singleton& s) = delete; // 防拷贝(注:delete是c++11中的语法),将该函数禁用
     Singleton* operator=(const Singleton& s) = delete; // 防赋值
@@ -13,5 +20,12 @@ public:
         return p;
     }
 };
+Singleton* Singleton::p = new Singleton(); // 第一次创建类就给分配资源
 
-Singleton* Singleton::p = new Singleton(); // 第一次常见类就给分配资源
+int main()
+{
+    Singleton* s=Singleton::getInstance();//不会创建对象
+
+    system("pause");
+    return 0;
+}
